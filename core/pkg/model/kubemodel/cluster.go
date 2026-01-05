a8bd347893901be7336ea9f5d7cc31f712a5d24f
aaliomer-ibm <aaliomer@ibm.com>
2026-01-05 12:46:12 -0500

    removed metadata

--- core/pkg/model/kubemodel/cluster.go
+++ core/pkg/model/kubemodel/cluster.go
@@ -8,15 +8,10 @@ import (
 
 // @bingen:generate:Cluster
 type Cluster struct {
-	UID          uuid.UUID `json:"uid"`          // @bingen:field[version=1]
-	Provider     Provider  `json:"provider"`     // @bingen:field[version=1]
-	Account      string    `json:"account"`      // @bingen:field[version=1]
-	Name         string    `json:"name"`         // @bingen:field[version=1]
-	Agent        string    `json:"agent"`        // @bingen:field[version=1]
-	AgentVersion string    `json:"agentVersion"` // @bingen:field[version=1]
-	K8sVersion   string    `json:"version"`      // @bingen:field[version=1]
-	ClusterType  string    `json:"type"`         // @bingen:field[version=1]
-	Region       string    `json:"region"`       // @bingen:field[version=1]
-	Start        time.Time `json:"start"`        // @bingen:field[version=1]
-	End          time.Time `json:"end"`          // @bingen:field[version=1]
+	UID      uuid.UUID `json:"uid"`      // @bingen:field[version=1]
+	Provider Provider  `json:"provider"` // @bingen:field[version=1]
+	Account  string    `json:"account"`  // @bingen:field[version=1]
+	Name     string    `json:"name"`     // @bingen:field[version=1]
+	Start    time.Time `json:"start"`    // @bingen:field[version=1]
+	End      time.Time `json:"end"`      // @bingen:field[version=1]
 }
